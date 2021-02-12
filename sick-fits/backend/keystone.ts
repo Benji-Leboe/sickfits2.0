9a7b704d2958f5b3653e152adc25bab442bdfd9f
Benji Leboe <benjamin.r.leboe@gmail.com>
2021-02-12 03:20:58 -0800

    config first schemas and set up cloudinary api

--- sick-fits/backend/keystone.ts
+++ sick-fits/backend/keystone.ts
@@ -1,31 +1,53 @@
 import 'dotenv/config';
 import { config, createSchema } from '@keystone-next/keystone/schema';
+import { createAuth } from "@keystone-next/auth";
+import { statelessSessions, withItemData } from "@keystone-next/keystone/session";
+import { Product } from "./schemas/Product";
+import { User } from "./schemas/User";
+import { ProductImage } from "./schemas/ProductImage";
 
 const databaseURL = process.env.DATABASE_URL;
 
 const sessionConfig = {
-  maxAge: 60 * 60 * 24 * 360,
+  maxAge: (60 * 60 * 24 * 360),
   secret: process.env.COOKIE_SECRET
 };
 
-export default config({
-  server: {
-    cors: {
-      origin: [process.env.FRONTEND_URL],
-      credentials: true
-    }
-  },
-  db: {
-    adapter: 'mongoose',
-    url: databaseURL,
-    // TODO: add seeding
-  },
-  lists: createSchema({
-    // TODO: add schema items
-  }),
-  ui: {
-    // TODO: change for roles
-    isAccessAllowed: _ => true,
-  },
-  // TODO: add sesh vals here
+const { withAuth } = createAuth({
+  listKey: 'User',
+  identityField: 'email',
+  secretField: 'password',
+  initFirstItem: {
+    fields: ['name', 'email', 'password'],
+    // TODO: add init roles
+  }
 });
+
+export default withAuth(
+  config({
+    server: {
+      cors: {
+        origin: [process.env.FRONTEND_URL],
+        credentials: true
+      }
+    },
+    db: {
+      adapter: 'mongoose',
+      url: databaseURL,
+      // TODO: add seeding
+    },
+    lists: createSchema({
+      // TODO: add schema items
+      User,
+      Product,
+      ProductImage,
+    }),
+    ui: {
+      // TODO: change for roles
+      isAccessAllowed: ({ session }) => !!session?.data,
+    },
+    session: withItemData(statelessSessions(sessionConfig), {
+      User: `id`
+    })
+  })
+)
